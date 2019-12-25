f1bc14e1977c94e682d04c4a8028184e9742177a
GASHIRA-E <dsteams@outlook.jp>
2019-12-26 02:18:34 +0900

    jest側にもalias追加

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-  "roots": [
-    "./__tests__"
+  roots: ['./__tests__'],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
   ],
-  "testMatch": [
-    "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
-  ],
-  "transform": {
-    "^.+\\.(ts|tsx)$": "ts-jest"
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
-}
+  moduleNameMapper: {
+    '^@utils(.*)$': '<rootDir>/src/ts/utils/$1',
+  },
+};
